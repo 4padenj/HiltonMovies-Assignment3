33b81c3d1cabc1a7085efd75f27e71ceef204627
4padenj <61246837+4padenj@users.noreply.github.com>
2021-02-03 11:49:07 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using HiltonMovies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiltonMovies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyMovies()
+        {
+            return View(TempStorage.Movies);
+        }
+        public IActionResult MyPodcast()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult NewMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewMovie(Movie newMovie)
+        {
+            TempStorage.AddMovie(newMovie);
+            return View("ThankYou", newMovie);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
